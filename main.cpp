ad3160ac95aba0ae178805c0b9be6029d7b784fd
LiamGetHub <157172169+LiamGetHub@users.noreply.github.com>
2024-04-26 01:37:03 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "cmath"
+
+#include "Hashtable.h"
+using namespace std;
+
+
+
+int main() {
+  HashTable hashtable;
+  hashtable.printTable();
+
+  std::cout << "--------------" << std::endl;
+  
+  hashtable.insert("five");
+  hashtable.insert("four");
+  hashtable.insert("six");
+  hashtable.insert("seven");
+  
+  hashtable.insert("nine");
+  hashtable.insert("two");
+  hashtable.insert("one");
+  hashtable.insert("three");
+  hashtable.insert("eight");
+
+  hashtable.insert("ten");
+  hashtable.insert("eleven");
+  hashtable.insert("twelve");
+  hashtable.insert("thirteen");
+  hashtable.insert("fourteen");
+
+  hashtable.insert("fifthteen");
+  hashtable.insert("sixteen");
+  hashtable.insert("seventeen");
+  hashtable.insert("eightteen");
+  hashtable.insert("nineteen");
+  hashtable.insert("twenty");
+  hashtable.insert("twentyone");
+  hashtable.insert("twentytwo");
+  hashtable.insert("twentythree");
+  hashtable.insert("twentyfour");
+  
+  
+  std::cout << "--------------" << std::endl;
+  
+  hashtable.printTable();
+  std::cout << "Item count: " << hashtable.getItemCount() << std::endl;
+  std::cout << "Table size: " << hashtable.getTableSize() << std::endl;
+  std::cout << "Load Factor: " << hashtable.getLoadFactor() << std::endl;
+  
+  if (hashtable.search("ten") == true) {
+    std::cout << "Found" << std::endl;
+  }
+  else {
+    std::cout << "Not found" << std::endl;
+
+  }
+}
