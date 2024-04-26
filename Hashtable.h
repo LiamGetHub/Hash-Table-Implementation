ce093085e1007eed7e216c9ba5299a2d49e339f1
LiamGetHub <157172169+LiamGetHub@users.noreply.github.com>
2024-04-26 01:38:32 -0700

    Create Hashtable.h

---
+++ Hashtable.h
@@ -0,0 +1,44 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <vector>
+#include <iostream>
+
+//using namespace std;
+
+class HashTable {
+
+  public:
+    HashTable();
+
+    void insert(std::string str);
+
+    void printTable();
+
+    int hashFunction(std::string, int);
+
+    unsigned int hash(std::string);
+
+    void rehash();
+
+    bool search(std::string str);
+
+
+    int getItemCount();
+    int getTableSize();
+    double getLoadFactor();
+
+  private:
+
+    int itemCount;
+    int tableSize;
+    double loadFactor;
+
+    std::vector<std::string> * table;
+
+};
+
+
+
+
+#endif
